833f1aad0d58fd813812b8a0326db321298d3101
Annyni <raindbowcat2022@gmail.com>
2024-10-07 14:23:34 +0300

    added sections portfolio and price

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      close = document.querySelector('.menu__close');
+      close = document.querySelector('.menu__close'),
+      counters = document.querySelectorAll('.skills__progress-counter'),
+      lines = document.querySelectorAll('.skills__progress-line span');
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
@@ -8,4 +10,10 @@ hamburger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   menu.classList.remove('active');
+});
+
+console.log(lines);
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
